f400413617daf20c42f0c893feaef24f1292e889
Lu Gao <lgao72@gatech.edu>
2020-12-20 13:03:32 -0800

    clean up middle of linked list

--- middle-of-linked-list/Solution.h
+++ middle-of-linked-list/Solution.h
@@ -1,33 +1,31 @@
 /*
-228. Middle of Linked List
-中文English
-Find the middle node of a linked list.
+    228. Middle of Linked List
 
-Example
-Example 1:
+    Find the middle node of a linked list.
 
-Input:  1->2->3
-Output: 2	
-Explanation: return the value of the middle node.
-Example 2:
+    Example
+    Example 1:
 
-Input:  1->2
-Output: 1	
-Explanation: If the length of list is  even return the value of center left one.
+    Input:  1->2->3
+    Output: 2	
+    Explanation: return the value of the middle node.
+    Example 2:
+
+    Input:  1->2
+    Output: 1	
+    Explanation: If the length of list is  even return the value of center left one.
 */
 
-/**
- * Definition of singly-linked-list:
- * class ListNode {
- * public:
- *     int val;
- *     ListNode *next;
- *     ListNode(int val) {
- *        this->val = val;
- *        this->next = NULL;
- *     }
- * }
- */
+class ListNode {
+public:
+    int val;
+    ListNode *next;
+    ListNode(int val) {
+        this->val = val;
+        this->next = NULL;
+    }
+    
+}
 
 class Solution {
 public:
