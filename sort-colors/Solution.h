66c2e5ac11065426d2a8185cae33ef9049023589
Lu Gao <sonomag13@gmail.com>
2021-08-17 16:33:25 -0700

    sort color updated

--- sort-colors/Solution.h
+++ sort-colors/Solution.h
@@ -1,53 +1,76 @@
+/**
+    75. Sort Colors
+    Medium
+
+    Given an array nums with n objects colored red, white, or blue, sort them in-place 
+    so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+    We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+    You must solve this problem without using the library's sort function.
+
+    Example 1:
+
+    Input: nums = [2,0,2,1,1,0]
+    Output: [0,0,1,1,2,2]
+    Example 2:
+
+    Input: nums = [2,0,1]
+    Output: [0,1,2]
+    Example 3:
+
+    Input: nums = [0]
+    Output: [0]
+    Example 4:
+
+    Input: nums = [1]
+    Output: [1]
+
+    Constraints:
+
+    n == nums.length
+    1 <= n <= 300
+    nums[i] is 0, 1, or 2.
+ **/
+
 #include <vector>
+
+using namespace std; 
 
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-
-        /**
-         * define of the intervals for 1, 2 and 3
-         * - [0, idx0): 0
-         * - [idx0, idx1): 1
-         * - [idx1, idx2): unsorted region
-         * - [idx2, nums.size()): 2          
-         **/
-
-        int idx0 = 0; 
-        int idx1 = 0; 
-        int idx2 = nums.size();                 
         
-        while (idx1 < idx2) {            
-            switch (nums[idx1]) {
+         /**        
+          * points the next position to accommondate 0
+          * interestingly, once having left the initial position, it always points to 1        
+          **/ 
+         int p0 = 0;             
+        
+         /**        
+          * points the next position to accommondate 2. 
+          * p2 + 1 always points to an element 2
+          **/
+        int p2 = nums.size() - 1;
+        
+        int k = 0;      // traverse through the array 
+        
+        while (k <= p2) {
+            
+            switch(nums[k]) {
                 case 0:
-                    /**
-                     * swap the current number and the first number
-                     * in the section of 2. 
-                     **/ 
-                    swap(nums[idx1], nums[idx0]); 
-                    idx0++; 
-                    idx1++;
-                    break;                     
+                    swap(nums[k], nums[p0]);
+                    p0++;
+                    k++;    
+                    break;
                 case 1:
-                    /**                     
-                     * increase idx2 by 1 since the current number
-                     * happens to be 2, so no swap is needed
-                     **/
-                    idx1++;    
-                    break;                                 
+                    k++;
+                    break;
                 case 2:
-                    /**
-                     * swap the current number and the number right 
-                     * before section of 3                 
-                     * 
-                     * Notice that idx1 does not increase for this time
-                     * since we do not know the new value at nums[idx1],
-                     * which could be either 0 or 1
-                     **/
-                    swap(nums[idx1], nums[idx2 - 1]);
-                    idx2--;  
+                    swap(nums[k], nums[p2]);
+                    p2--;
                     break;                    
             }
-        }
+        }        
     }
-
 };
