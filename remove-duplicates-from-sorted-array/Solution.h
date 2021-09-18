71fcda78610f86c9c834ad8db48845cade1ad3b3
Lu Gao <44305906+sonomag13@users.noreply.github.com>
2021-09-17 22:21:16 -0700

    remove duplicates from sorted array

---
+++ remove-duplicates-from-sorted-array/Solution.h
@@ -0,0 +1,26 @@
+// LeetCode 26. Remove Duplicates from Sorted Array
+// Easy
+
+#include <vector>
+
+using namespace std; 
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (!nums.size()) {
+            return 0;
+        }
+        
+        int i = 0;
+        for (int j = 1; j < nums.size(); j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                swap(nums[i], nums[j]);
+            }
+        }
+        
+        return i + 1;
+        
+    }
+};
